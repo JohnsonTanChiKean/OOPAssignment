4172168403fbf639cb4a14f760aa15305566a1bd
Lim ZH <Lim ZH@192.168.0.108>
2022-08-21 00:05:02 +0800

    xxxx

--- Assignment/src/Receipt.java
+++ Assignment/src/Receipt.java
@@ -26,7 +26,7 @@ public class Receipt {
 	}
 	
 	public String toString() {
-		String random2;
+		String random3;
 		String receiptDetails="";
 		receiptDetails+=String.format("-----------------------------------------------------------------------------------\n");
 		receiptDetails+=String.format("| Receipt ID: %-67s |\n", getFullRecID());
