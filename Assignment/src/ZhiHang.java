b9d661fe57c6b7a24563d8c40a905fe7fa68521a
ser <ser@LAPTOP-14K0V2C3>
2022-08-25 10:43:37 +0800

    Added prototype Menu

--- Assignment/src/ZhiHang.java
+++ Assignment/src/ZhiHang.java
@@ -2,61 +2,93 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ZhiHang {
-	public static void login(ArrayList<Staff> staffList, ArrayList<Product> productList, ArrayList<Member> memberList, ArrayList<Payment> paymentList, ArrayList<Receipt> receiptList, BankAccount bankAccount) {
+	public static void login(ArrayList<Staff> staffList, ArrayList<Product> productList, ArrayList<Member> memberList, ArrayList<Payment> paymentList, ArrayList<Receipt> receiptList, BankAccount bankAccount, ArrayList<Refund> refundList) {
 
-		int found=0;
+		int found=0, loop=0;
 		char choice;
 		Scanner scanner=new Scanner(System.in);
 		int idNo=0;
 		Staff staff;
-		System.out.println("Welcome to Connect POS System");
 		
 		do {
-			System.out.print("Enter staff ID: ");
-			String staffID=scanner.next();
-			scanner.nextLine();
-			staffID=staffID.toUpperCase();
-			if(staffID.length()!=5) {
-				System.out.println("Invalid Staff ID! Staff ID must have 5 digits");
-			}
-			else {
-				idNo=Integer.parseInt(staffID.replace("S", ""));
-				for(Staff i: staffList) {
-					if(idNo==i.getIdNo()) {
-						found=1;
+			System.out.println("Welcome to Connect POS System");
+			
+			do {
+				System.out.print("Enter staff ID: ");
+				String staffID=scanner.next();
+				scanner.nextLine();
+				staffID=staffID.toUpperCase();
+				if(staffID.length()!=5) {
+					System.out.println("Invalid Staff ID! Staff ID must have 5 digits");
+				}
+				else {
+					idNo=Integer.parseInt(staffID.replace("S", ""));
+					for(Staff i: staffList) {
+						if(idNo==i.getIdNo()) {
+							found=1;
+						}
+					}
+				}
+				if(found==0) {
+					System.out.println("Staff ID not found. Please try again");
+					System.out.print("Would you like to try again? (Y=yes) ");
+					choice=Character.toUpperCase(scanner.next().charAt(0));
+					scanner.nextLine();
+				}
+				
+			}while(found==0);
+			
+			if(found==1) {
+				System.out.print("Enter Password: ");
+				String password=scanner.next();
+				scanner.nextLine();
+				for(int i=0; i<staffList.size(); i++) {
+					if(idNo==staffList.get(i).getIdNo()&&password.equals(staffList.get(i).getPassword())) {
+						System.out.println("Successful login");
+						System.out.println("Welcome "+staffList.get(i).getName());
+						
+						mainMenu(staffList, productList, memberList, staffList.get(i), receiptList, paymentList, bankAccount, refundList);
 					}
 				}
 			}
-			if(found==0) {
-				System.out.println("Staff ID not found. Please try again");
-				System.out.print("Would you like to try again? (Y=yes) ");
-				choice=Character.toUpperCase(scanner.next().charAt(0));
-				scanner.nextLine();
+		}while(loop==0);
+		
+	}
+	
+	public static void mainMenu(ArrayList<Staff> staffList, ArrayList<Product> productList, ArrayList<Member> memberList, Staff staff, ArrayList<Receipt> receiptList, ArrayList<Payment> paymentList, BankAccount bankAccount, ArrayList<Refund> refundList) {
+		Scanner scanner=new Scanner(System.in);
+		int choice=0, loop=0;
+		char quitChoice;
+		
+		do {
+			choice=0;
+			loop=0;
+			System.out.println("Main Menu");
+			System.out.println("1. Place Order");
+			System.out.println("0. Logout");
+			System.out.print("Select Action to perform: ");
+			choice=scanner.nextInt();
+			scanner.nextLine();
+			
+			switch(choice) {
+			case 1: ChiKean.cart(staffList,productList,memberList, paymentList, staff, receiptList, bankAccount); loop=1; break;
+			case 0: break;
 			}
 			
-		}while(found==0);
-		
-		if(found==1) {
-			System.out.print("Enter Password: ");
-			String password=scanner.next();
-			scanner.nextLine();
-			for(int i=0; i<staffList.size(); i++) {
-				if(idNo==staffList.get(i).getIdNo()&&password.equals(staffList.get(i).getPassword())) {
-					System.out.println("Successful login");
-					System.out.println("Welcome "+staffList.get(i).getName());
-					
-					menuList(staffList, productList, memberList, staffList.get(i), receiptList, paymentList, bankAccount);
+			if(loop==0) {
+				System.out.print("Are you sure you want to logout? (Y=yes)");
+				quitChoice=Character.toUpperCase(scanner.next().charAt(0));
+				scanner.nextLine();
+				
+				if(quitChoice!='Y') {
+					loop=1;
 				}
 			}
-		}
-	}
-	
-	public static void menuList(ArrayList<Staff> staffList, ArrayList<Product> productList, ArrayList<Member> memberList, Staff staff, ArrayList<Receipt> receiptList, ArrayList<Payment> paymentList, BankAccount bankAccount) {
-		ChiKean.cart(staffList,productList,memberList, paymentList, staff, receiptList, bankAccount);
-	}
-	
-	public static void testingZH2() {
+		}while(loop==1);
+		
+		
 		
 	}
+	
 	
 }
